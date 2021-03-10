1005b7a0f24ef9728be598b831f0f84bfeeb60b3
yqylh <1071665725@qq.com>
2021-03-11 00:02:31 +0800

    init

---
+++ SDK_C++/CodeCraft-2021/CodeCraft-2021.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "1.cpp"
+int main()
+{
+	test();
+	// TODO:read standard input
+	// TODO:process
+	// TODO:write standard output
+	// TODO:fflush(stdout);
+
+	return 0;
+}
---
+++ SDK_C++/CodeCraft-2021/lib/1.cpp
@@ -0,0 +1,5 @@
+#include <cstdio>
+
+void test() {
+    printf("test\n");
+}
