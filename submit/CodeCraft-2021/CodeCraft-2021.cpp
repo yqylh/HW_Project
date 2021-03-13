274e862b47cbaea97b0a5e44ec4a47b59b228f22
yqylh <1071665725@qq.com>
2021-03-13 17:46:07 +0800

    move 解耦

--- submit/CodeCraft-2021/CodeCraft-2021.cpp
+++ submit/CodeCraft-2021/CodeCraft-2021.cpp
@@ -252,68 +252,113 @@ void createDoubleServer(request & req, virtualServer & virSerType) {
 		buyServer(req, virSerType, 1);
 	}
 }
+// 迁移行动
+void moveAction(moveMark x, int day) {
+	moveList[day].push_back(moveMark(x.virSerId, x.serId, x.where));
+	auto & virSer = virtualServerList[x.virSerId];
+	auto & ser = serverList[x.serId];
+	auto & before = serverList[virSer.serverId];
+	// 单核心迁移
+	if (virSer.isDouble == 0) {
+		//debug
+		// if (virSer.serverId == ser.id && virSer.where == x.where ) {
+		// 	printf("%d %d -> %d %d\n" , virSer.serverId, virSer.where , ser.id, x.where);
+		// }
+		//
+		// 迁移到左边
+		if (x.where == 0) {
+			ser.lCore -= virSer.core;
+			ser.lRam -= virSer.ram;
+			if (virSer.where == 0) {
+				before.lCore += virSer.core;
+				before.lRam += virSer.ram;
+			} else {
+				before.rCore += virSer.core;
+				before.rRam += virSer.ram;
+			}
+			virSer.serverId = ser.id;
+			virSer.where = 0;
+		} else { // 迁移到右边
+			ser.rCore -= virSer.core;
+			ser.rRam -= virSer.ram;
+			if (virSer.where == 0) {
+				before.lCore += virSer.core;
+				before.lRam += virSer.ram;
+			} else {
+				before.rCore += virSer.core;
+				before.rRam += virSer.ram;
+			}
+			virSer.serverId = ser.id;
+			virSer.where = 1;
+		}
+	} else { // 双核心迁移
+		ser.lCore -= virSer.core / 2;
+		ser.lRam -= virSer.ram / 2;
+		ser.rCore -= virSer.core / 2;
+		ser.rRam -= virSer.ram / 2;
+
+		before.lCore += virSer.core / 2;
+		before.lRam += virSer.ram / 2;
+		before.rCore += virSer.core / 2;
+		before.rRam += virSer.ram / 2;
+
+		virSer.serverId = ser.id;
+	}
+}
 
 // 迁移策略
 void move(int day) {
 	int maxMoveNum = virtualServerList.size() * 5 / 1000;
 	for (auto & i : virtualServerList) if (maxMoveNum) {
-		for (auto & j : serverList) {
-			auto & before = serverList[i.second.serverId];
-			if (i.second.isDouble == 0) {
-				 // 策略
-				 // 左右核心可以互相移动☆
-				 if (j.second.lCore > i.second.core && j.second.lRam > i.second.ram && (i.second.serverId != j.first || i.second.where == 1)) {
-					j.second.lCore -= i.second.core;
-					j.second.lRam -= i.second.ram;
-					if (i.second.where == 0) {
-						before.lCore += i.second.core;
-						before.lRam += i.second.ram;
+		auto & before = serverList[i.second.serverId];
+
+		int moveServerId; // 返回变量
+		moveServerId = -1;
+		bool lorR = 0; // 0代表左边 1代表右边
+		
+		if (i.second.isDouble == 0) {
+			for (auto & j : serverList) {
+				if (j.second.lCore > i.second.core && j.second.lRam > i.second.ram ) {
+					if (i.second.serverId == j.first && i.second.where == 0) continue;
+					// 策略零
+					if (moveServerId == -1) {
+						moveServerId = j.first;
+						lorR = 0;
 					} else {
-						before.rCore += i.second.core;
-						before.rRam += i.second.ram;
+						// 策略一 
 					}
-					i.second.serverId = j.first;
-					i.second.where = 0;
-					moveList[day].push_back(moveMark(i.first, j.first, 0));
-					maxMoveNum--;
-					break;
-				 }
-				 if (j.second.rCore > i.second.core && j.second.rRam > i.second.ram && (i.second.serverId != j.first || i.second.where == 0)) {
-					j.second.rCore -= i.second.core;
-					j.second.rRam -= i.second.ram;
-					if (i.second.where == 0) {
-						before.lCore += i.second.core;
-						before.lRam += i.second.ram;
+				}
+				if (j.second.rCore > i.second.core && j.second.rRam > i.second.ram ) {
+					if (i.second.serverId == j.first && i.second.where == 1) continue;
+					// 策略零
+					if (moveServerId == -1) {
+						moveServerId = j.first;
+						lorR = 1;
 					} else {
-						before.rCore += i.second.core;
-						before.rRam += i.second.ram;
+						// 策略一 
 					}
-					i.second.serverId = j.first;
-					i.second.where = 1;
-					moveList[day].push_back(moveMark(i.first, j.first, 1));
-					maxMoveNum--;
-					break;
-				 }
-			} else {
-				// 策略
+				}
+			}
+			if (moveServerId != -1) {
+				moveAction(moveMark(i.first, moveServerId, lorR), day);
+				maxMoveNum--;
+			}
+		} else {
+			// 策略
+			for (auto & j : serverList) {
 				if (j.second.lCore > i.second.core && j.second.lRam > i.second.ram &&
 					j.second.rCore > i.second.core && j.second.rRam > i.second.ram && i.second.serverId != j.first) {
-
-					j.second.lCore -= i.second.core / 2;
-					j.second.lRam -= i.second.ram / 2;
-					j.second.rCore -= i.second.core / 2;
-					j.second.rRam -= i.second.ram / 2;
-
-					before.lCore += i.second.core / 2;
-					before.lRam += i.second.ram / 2;
-					before.rCore += i.second.core / 2;
-					before.rRam += i.second.ram / 2;
-
-					i.second.serverId = j.first;
-					moveList[day].push_back(moveMark(i.first, j.first, -1));
-					maxMoveNum--;
-					break;
+					// 策略零
+					if (moveServerId == -1) {
+						moveServerId = j.first;
+					} else {
+						// 策略一 
+					}
 				}
+			}
+			if (moveServerId != -1) {
+				moveAction(moveMark(i.first, moveServerId, -1), day);
+				maxMoveNum--;
 			}
 		}
 		break;
