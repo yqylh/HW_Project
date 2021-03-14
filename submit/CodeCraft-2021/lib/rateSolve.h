6088f32e1081f0814ffc682c9f3534ec851797bd
yqylh <1071665725@qq.com>
2021-03-14 18:08:21 +0800

    fix move bug !! && 第二了 最新baseline 1133242551

--- submit/CodeCraft-2021/lib/rateSolve.h
+++ submit/CodeCraft-2021/lib/rateSolve.h
@@ -3,14 +3,20 @@
 struct rateSolve{
     int id;
     double rate;
+    int lorR;
     std::vector<int> virSerIds;
     rateSolve(){
         id = 0;
         rate = 0;
+        lorR = -1;
         virSerIds.clear();
     }
     ~rateSolve() {}
     rateSolve(int id, double rate) : id(id), rate(rate) {
+        lorR = -1;
+        virSerIds.clear();
+    }
+    rateSolve(int id, double rate, bool lorR) : id(id), rate(rate) , lorR(lorR) {
         virSerIds.clear();
     }
     inline friend bool operator<(const rateSolve &A, const rateSolve &B){
