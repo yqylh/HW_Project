a056352f670e2ac9cd8ce5f8a5c239e9727312f1
yqylh <1071665725@qq.com>
2021-03-24 00:47:44 +0800

    request sorted  1108372739

---
+++ Src/lib/CreateType.h
@@ -0,0 +1,14 @@
+struct CreateType{
+    int _serverId;
+    int _type;
+    int order;
+    CreateType() {
+        _serverId = 0;
+        _type = 0;
+        order = 0;
+    }
+    CreateType(int _serverId, int _type, int order) : _serverId(_serverId), _type(_type), order(order) {}
+    inline bool friend operator< (const CreateType &A, const CreateType &B) {
+        return A.order < B.order;
+    }
+};
